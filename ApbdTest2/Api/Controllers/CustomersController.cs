957c288580b0df94f042fb847d58f2ab93b18600
mlaskowski7 <mtlaskowski7@gmail.com>
2025-06-09 17:18:27 +0200

    fix: use created response without location

--- ApbdTest2/Api/Controllers/CustomersController.cs
+++ ApbdTest2/Api/Controllers/CustomersController.cs
@@ -51,7 +51,7 @@ public class CustomersController(ICustomerService customerService) : ControllerB
                 return Conflict($"Customer cant purchase more than 5 tickets for a concert");
             }
 
-            return CreatedAtAction(nameof(GetCustomerPurchasesById), customer);
+            return StatusCode(201, customer);
         }
         catch (ConflictException ex)
         {
